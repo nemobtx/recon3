cf313bc8d64b08ed3dac9f11bcfcb4856305510e
Yongduek Seo <yndk@sogang.ac.kr>
2015-03-03 09:55:34 +0900

    removed: DerivedData

--- DerivedData/sfm/Build/Intermediates/sfm.build/Debug/sfm.build/Objects-normal/x86_64/SfMUpdateListener.d
+++
@@ -1,7 +0,0 @@
-dependencies: /Users/yndk/Dropbox/recon3/sfm/SfMUpdateListener.cpp \
-  /Users/yndk/Dropbox/recon3/sfm/SfMUpdateListener.h \
-  /usr/local/include/opencv2/core/core.hpp \
-  /usr/local/include/opencv2/core/types_c.h \
-  /usr/local/include/opencv2/core/version.hpp \
-  /usr/local/include/opencv2/core/operations.hpp \
-  /usr/local/include/opencv2/core/mat.hpp
